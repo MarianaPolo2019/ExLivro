18fde48ed40414fd7594560f46a073c2be1071fd
MarianaPolo2019 <mary.polo@hotmail.com>
2021-04-16 20:07:38 -0300

    Initial commit

---
+++ Data/IESContext.cs
@@ -0,0 +1,19 @@
+﻿using ExLivro.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExLivro.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ Models/Livro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExLivro.Models
+{
+    public class Livro
+    {
+
+        [Key]
+        public long? LivroID { get; set; }
+
+       
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Autor { get; set; }
+      
+        [Required]
+        public string Editora { get; set; }
+        [Display(Name = "Quantidade de Páginas")]
+        [Required]
+        public int QtdePag { get; set; }
+        
+        [Display(Name = "Capa Dura")]
+        [Required]
+        public bool CapaDura { get; set; }
+        [Display(Name = "Ano Publicação")]
+        [Required]
+        public int AnoPub { get; set; }
+        
+        [Required]
+        public float Valor { get; set; }
+    }
+}
